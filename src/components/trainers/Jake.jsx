b21945264e0c3e61bc1fae5b14a549bb8d58017f
Jacob Flora <jacobflora4@gmail.com>
2021-12-20 13:54:54 -0500

    fixed typo in Jake description

--- src/components/trainers/Jake.jsx
+++ src/components/trainers/Jake.jsx
@@ -8,7 +8,7 @@ function Jake() {
         <h1>Jacob Flora</h1>
         <br />
         <p className='trainer-description'>"I didn't take my 20's seriously. I had fun, drank, smoked, partied, etc... 
-        Now I've gotten my life under control. I've quit drinking and smoking. I've lose over 40 lbs and I'm still going.
+        Now I've gotten my life under control. I've quit drinking and smoking. I've lost over 40 lbs and I'm still going.
         I wan't to help people get healthier and reach their goals. I know what it takes because I am just like you!"</p>
       </figcaption>
     </div>
